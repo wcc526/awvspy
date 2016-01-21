4d92c3744a8e5bd283663fa5ae7e8bdc447e4c7b
wcc526 <949409306@qq.com>
2016-01-21 15:44:01 +0800

    update models

--- awvspy/awvsmodels.py
+++ awvspy/awvsmodels.py
@@ -11,7 +11,7 @@ LOG = logging.getLogger(__name__)
 from sqlalchemy.dialects import registry
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column
-from awvspy.packages.sqlalchemy_access.sqlalchemy_access.base import AcInteger
+from awvspy.packages.sqlalchemy_access.sqlalchemy_access.base import AcInteger,AcString,AcDateTime,AcBoolean
 
 registry.register("access", "sqlalchemy_access.pyodbc", "AccessDialect_pyodbc")
 registry.register("access.pyodbc", "sqlalchemy_access.pyodbc", "AccessDialect_pyodbc")
@@ -23,6 +23,13 @@ class WVSScans(BASE):
     __tablename__ = 'WVS_scans'
 
     scid = Column(AcInteger,primary_key=True)
+    grid = Column(AcInteger)
+    starturl = Column(AcDateTime)
+    starttime = Column(AcDateTime)
+    finishtime = Column(AcDateTime)
+    aborted = Column(AcBoolean)
+    responsive = Column(AcBoolean)
+    serverid = Column(AcInteger)
 
     def __repr__(self):
-        return "%s" % (self.scid)
+        return "%s|%s|%s|%s|%s|%s|%s|%s" % (self.scid,self.grid,self.starturl,self.starttime,self.finishtime,self.aborted,self.responsive,self.serverid)
